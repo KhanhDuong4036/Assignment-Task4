5ea8f468428775414e6f26af6090094da50e2fe8
Khanh Duong <khanhduong4036@gmail.com>
2022-08-31 17:04:45 +0700

    Update Task 4

--- Core/src/main/java/task/core/CucumberRunnerBase.java
+++ Core/src/main/java/task/core/CucumberRunnerBase.java
@@ -7,7 +7,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 
-@Listeners({ScreenshotListener.class, PropertyListener.class, ReportListener.class})
+
 public class CucumberRunnerBase extends AbstractTestNGCucumberTests {
 
     @DataProvider
